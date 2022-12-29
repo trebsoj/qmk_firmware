11df9804299775beabe95e4f39349b03f76b75f0
Josbert Pedrol <josbert.pedrol@bassolsenergia.com>
2022-12-29 17:05:15 +0100

    changes

--- keyboards/bm40hsrgb/keymaps/trebsoj/keymap.c
+++ keyboards/bm40hsrgb/keymaps/trebsoj/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* CALC */
 [2] = LAYOUT_planck_mit(
-    KC_TRNS, KC_NO,      KC_NO,         KC_NO,      KC_NO,        KC_NO, KC_PERC,     KC_P7,    KC_P8,    KC_P9,    KC_PPLS, KC_TRNS,
+    KC_NUMLOCK, KC_NO,      KC_NO,         KC_NO,      KC_NO,        KC_NO, KC_PERC,     KC_P7,    KC_P8,    KC_P9,    KC_PPLS, KC_TRNS,
     KC_TRNS, LCTL(KC_A),      KC_NO,         KC_NO,      KC_NO,        KC_NO,      KC_CIRC,     KC_P4,    KC_P5,    KC_P6,    KC_PMNS, LALT(KC_ENT),
     KC_TRNS, KC_NO,      KC_NO,         LCA(KC_C),      LCA(KC_V),        KC_NO,      KC_PCMM,     KC_P1,    KC_P2,    KC_P3,    KC_PAST, KC_TRNS,
     KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS,    MO(4),        KC_TRNS,                 KC_P0,    KC_PDOT,  KC_COMM,  KC_PSLS, KC_EQL
